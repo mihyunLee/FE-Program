77ba04684c35f0344813e2ba7a1b6967cd2e7dec
mihyunLee <mihy621@gmail.com>
2022-07-22 11:20:06 +0900

    feat: #4 - 승리의 원인이 되는 사각형 강조

--- React/tic-tac-toe/src/index.js
+++ React/tic-tac-toe/src/index.js
@@ -2,21 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Square(props) {
+function Square({ value, onClick, style }) {
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
+    <button className="square" onClick={onClick} style={style}>
+      {value}
     </button>
   );
 }
 
 class Board extends React.Component {
   renderSquare(i) {
+    const isWinningIndex =
+      this.props.winningIndex && this.props.winningIndex.indexOf(i) !== -1;
+    const styleObj = {
+      boxShadow: "0px 2px 8px 0px #FFC233",
+      border: "2px solid black",
+    };
     return (
       <Square
         key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        style={isWinningIndex ? styleObj : null}
       />
     );
   }
@@ -60,10 +67,13 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice(); // .slice() 로 squares 배열의 복사본을 생성하여 수정한다.
+
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+
     squares[i] = this.state.xIsNext ? "X" : "O";
+
     this.setState({
       history: history.concat([
         {
@@ -92,7 +102,6 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
-
     const moves = history.map((step, move) => {
       const desc = move ? "Go to move #" + move : "Go to game start";
       return (
@@ -108,8 +117,10 @@ class Game extends React.Component {
     });
 
     let status;
+    let winningIndex;
     if (winner) {
-      status = "Winner: " + winner;
+      status = "Winner: " + winner.player;
+      winningIndex = winner.winningIndex;
     } else {
       status = "Current player: " + (this.state.xIsNext ? "X" : "O");
     }
@@ -118,6 +129,7 @@ class Game extends React.Component {
       <div className="game">
         <div className="game-board">
           <Board
+            winningIndex={winningIndex}
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
@@ -155,7 +167,10 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        player: squares[a],
+        winningIndex: [a, b, c],
+      };
     }
   }
   return null;
